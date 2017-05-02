468d7b3b28075f327e417cb319f25599826be6f5
bowling-brawls <bowling.brawls@gmail.com>
2017-05-02 12:54:46 -0500

    verdoconductividad2

---
+++ newData.R
@@ -0,0 +1,25 @@
+library(xlsx)
+
+if(R.version$os=="linux-gnu"){ 
+  load("~/Documents/lechuga/noRawdfs.RData")
+} else if (R.version$os=="darwin15.6.0"){
+  load("~/Documents/Biologia/Tesis/dataAnalysis/noRawdfs.RData")
+}
+
+PE <- read.xlsx("/Users/macintoshhd/Documents/Biologia/Tesis/dataAnalysis/DatosTesis2.xlsx", 
+                   sheetIndex = 1, header = TRUE, 
+                  # rowIndex =1:37,
+                   stringsAsFactors=FALSE)
+
+verdosidad <- read.xlsx("/Users/macintoshhd/Documents/Biologia/Tesis/dataAnalysis/DatosTesis2.xlsx", 
+                sheetIndex = 3, header = TRUE, 
+                # rowIndex =1:37,
+                stringsAsFactors=FALSE)
+
+clima <- read.xlsx("/Users/macintoshhd/Documents/Biologia/Tesis/dataAnalysis/DatosTesis2.xlsx", 
+                sheetIndex = 4, header = TRUE, 
+                # rowIndex =1:37,
+                stringsAsFactors=FALSE)
+
+save(HojasConRGR, PE, verdosidad, clima,
+     file= "~/Documents/Biologia/Tesis/dataAnalysis/noRawdfs.RData") 
