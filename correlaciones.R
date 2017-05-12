3012f5c8e8254e51e4dd9daac7eb6becde0c5e6f
NicolasMorenoAndrade <uselessconquest@gmail.com>
2017-05-12 16:26:58 -0500

    grabar correlaciones.R

--- correlaciones.R
+++ correlaciones.R
@@ -4,9 +4,16 @@ colsSE <- c( "fv.fm", "qP", "NPQ","a.fol", "AFE", "IAF", "y.II.", "etr",
 
 library(corrgram)
 library(Hmisc)
+library(xlsx)
 
 corrgram(HojasConRGR[,colsSE],order = TRUE, upper.panel = NULL)
-correlaciones <- rcorr(as.matrix(HojasConRGR[,colsSE]), 
-                       as.matrix(HojasConRGR[,colsSE]), type="p")
-correlaciones <-  as.matrix(correlaciones$V1$r)
+correlaciones <- rcorr(as.matrix(HojasConRGR[,colsSE]), as.matrix(HojasConRGR[,colsSE]), 
+                       type="p")
 
+correlacionesPearson <- as.data.frame(correlaciones$r)
+
+significanciaPearson <- as.data.frame(correlaciones$P)
+
+save(significanciaPearson, correlacionesPearson, file="correlaciones.RData")
+write.xlsx(correlacionesPearson[1:12,1:12], file = "corrPearson.xlsx")
+write.xlsx(significanciaPearson[1:12,1:12], file = "signPearson.xlsx")
